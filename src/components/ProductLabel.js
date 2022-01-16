7824ee0357f641a535f450ac99dcb64c78f1be62
peterkim1994 <52155345+peterkim1994@users.noreply.github.com>
2022-01-17 00:30:54 +1300

    fixed label printing bug

--- src/components/ProductLabel.js
+++ src/components/ProductLabel.js
@@ -3,11 +3,11 @@ import { MedLabel, LabelFont } from '../util/LabelDimensions';
 
 const ProductLabel = ({ product, size, productId, isSmallLabel }) => {
 
-    if(product == null){        
-        let url_string = window.location.href;
-        let url = new URL(url_string);
-        productId = url.searchParams.get("productId");
-    }
+   // if(product == null){        
+     //   let url_string = window.location.href;
+     //   let url = new URL(url_string);
+    //    productId = url.searchParams.get("productId");
+   // }
 
     let labelTag = <span>
                         {product.brandValue}&nbsp;{product.itemCategoryValue}&nbsp;
@@ -19,23 +19,26 @@ const ProductLabel = ({ product, size, productId, isSmallLabel }) => {
     }
 
     let labelText = `[${product.brandValue}][${product.itemCategoryValue}][${product.colourValue}][${product.sizeValue}] \$${product.price}`;
-
-  
-
+    if(product.itemCategoryValue == "Sunglasses"){
+       labelText = `[${product.brandValue}][${product.itemCategoryValue}][${product.colourValue}][${product.manufactureCode}] \$${product.price}`;
+    }
+   
+    let barcodeNumber =  product.barcode.toString();
     const barcodeRef = useRef();
     useEffect(() => {
         let barcodeHeight = (labelText.length > 45) ? 25 : 38;
-        var JsBarcode = require('jsbarcode');
+        let JsBarcode = require('jsbarcode');
         JsBarcode(".barcode").options({height:35, width:1.8, fontSize:9, textPosition:"top", marginTop:9, marginBottom:1, marginRight:5, marginLeft:4} ).init()
-    }, []);
+    });
 
     return (        
-        <div id={`product-label-${product.id}`} className="product-label" style={{ ...MedLabel}}>       
-                <svg className="barcode"                    
-                    jsbarcode-value={product.barcode.toString()}
+        <div id={`product-label-${product.id}`} className={"product-label"} style={{ ...MedLabel}}>  
+                <span></span>     
+                <svg className= "barcode"                   
+                    jsbarcode-value={barcodeNumber}
                     jsbarcode-textmargin="0"    
                     jsbarcode-fontoptions="light"
-                    JsBarcode-fontSize="8"
+                    jsBarcode-fontSize="8"
                     >
                 </svg>               
                 <div style={{...LabelFont}}>
